7fbe9931e4391a1169558261ba32d7c6d7f06c3e
godsakani <nyonggodwill11@gmail.com>
2022-07-27 15:12:04 +0100

    feat: modified message_widget

--- lib/widgets/message_widget.dart
+++ lib/widgets/message_widget.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import '../widgets/streammessage_widget.dart';
 
 class MessageBubble extends StatelessWidget {
   const MessageBubble({
     Key? key,
     required this.sender,
     required this.text,
+    required this.isMe,
   }) : super(key: key);
 
   final String sender;
   final String text;
+  final bool isMe;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(
             sender,
@@ -24,9 +28,19 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(20.0),
+            borderRadius: isMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
             elevation: 5.0,
-            color: Color(0xFF71ae87),
+            color: isMe ? const Color(0xFF71ae87) : Colors.white,
             child: Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 12,
@@ -34,8 +48,8 @@ class MessageBubble extends StatelessWidget {
               ),
               child: Text(
                 text,
-                style: const TextStyle(
-                  color: Colors.white,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
                   fontSize: 15,
                 ),
               ),
